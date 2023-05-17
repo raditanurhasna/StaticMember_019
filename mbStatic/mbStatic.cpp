2eb6bce88c513415efdb4f3ee5000731adf04755
raditanurhasna <raditanurhasna1410@gmail.com>
2023-05-17 15:36:09 +0700

    Membuat Mahasiswa

---
+++ mbStatic/mbStatic.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa {
+public:
+	static int nim;
+	int id;
+	string nama;
+
+	void setID();
+	void printAll();
+	mahasiswa(string pnama) :nama(pnama) { setID(); }
+};
+
+
+
