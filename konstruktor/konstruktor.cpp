08269920ce05ddad861d8a2f594a203558caa1a0
raditanurhasna <raditanurhasna1410@gmail.com>
2023-05-17 17:38:48 +0700

    Menampilkan data Mahasiswa

--- konstruktor/konstruktor.cpp
+++ konstruktor/konstruktor.cpp
@@ -37,3 +37,17 @@ void mahasiswa::cetak() {
 	cout << endl << "Nim = " << nim << endl;
 	cout << " Nama = " << nama << endl;
 }
+
+int main() {
+	mahasiswa mhs1;
+	mahasiswa mhs2(20);
+	mahasiswa mhs3("indra");
+	mahasiswa mhs4(30, "fauzan");
+
+	mhs1.cetak();
+	mhs2.cetak();
+	mhs3.cetak();
+	mhs4.cetak();
+
+	return 0;
+}
