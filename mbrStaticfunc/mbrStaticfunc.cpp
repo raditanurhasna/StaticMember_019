ed3fe591c76048c4d4fe7028cfc972a42ded15ca
raditanurhasna <raditanurhasna1410@gmail.com>
2023-05-17 17:56:54 +0700

    digunakan untuk mengilustrasikan penggunaan kelas mahasiswa dan mengelola data mahasiswa seperti identitas dan nama dengan bantuan variabel statis nim.

---
+++ mbrStaticfunc/mbrStaticfunc.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+class mahasiswa {
+private:
+	static int nim;
+public:
+	int id;
+	string nama;
+
+	void setID();
+	void printAll();
+
+	static void setNim(int pNim) { nim = pNim;  }
+	static int getNiM() { return nim; }
+
+	mahasiswa(string pnama) :nama(pnama) { setID(); }
+};
+
+int mahasiswa::nim = 0;
+
+void mahasiswa::setID() {
+	id = ++nim;
+}
+
+void mahasiswa::printAll() {
+	cout << "ID = " << id << endl;
+	cout << "Nama = " << nama << endl;
+	cout << endl;
+
+}
+
+int main() {
+	mahasiswa mhs1("sri dadi");
+	mahasiswa mhs2("Budi Jatmiko");
+	mahasiswa::setNim(9);
+	mahasiswa mhs3("Andi Janu");
+	mahasiswa mhs4("Joko Wahono");
+
+	mhs1.printAll();
+	mhs2.printAll();
+	mhs3.printAll();
+	mhs4.printAll();
+
+	cout << "akses dari luar object = " << mahasiswa::getNiM() << endl;
+	
+
+		return 0;
+
+}
